694635b8e1465455aa75582c14e3c48486d22add
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-02-06 10:44:41 +0100

    Exercise 3

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 async function fetchData(url) {
-  const response = await fetch(
-    `${url}`
-  );
-  const pokemons = await response.json();
-  
-  pokemons.forEach(element => {
-    console.log(element);
-    renderTheData(element.name)
-  });
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
+
+const writeToConsole = (data) => {
+  data.forEach((element) => {
+    console.log(element);
+  });
+};
 
 const renderTheData = (data) => {
-    let pokemonDiv = document.getElementById("pokemon-div");
-    let element = document.createElement("p")
-    pokemonDiv.appendChild(element)
-    element.innerText= data
+  data.forEach((element) => {
+    console.log(element);
+    createDomElement(element.name);
+  });
+};
 
+const createDomElement = (data) => {
+  let pokemonDiv = document.getElementById("pokemon-div");
+  let element = document.createElement("p");
+  pokemonDiv.appendChild(element);
+  element.innerText = data;
+};
+
+const renderFilteredBooks = (books) => {
+      books.forEach((element) => {
+        if(element.pages < 500) {
+        createDomElement(element.title);
+        }
+      });
 }
 
-fetchData("https://majazocom.github.io/Data/pokemons.json");
-fetchData("https://majazocom.github.io/Data/dogs.json");
+(async () => {
+  let pokemons = await fetchData("https://majazocom.github.io/Data/pokemons.json"  );
+  writeToConsole(pokemons);
+  renderTheData(pokemons);
+
+  let dogs = await fetchData("https://majazocom.github.io/Data/dogs.json");
+  writeToConsole(dogs);
+  renderTheData(dogs);
+
+  let books = await fetchData("https://majazocom.github.io/Data/books.json");
+  renderFilteredBooks(books)
+})();
